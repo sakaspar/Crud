389a1c9d2fcb3e585a84ec9e7db4292b0e4a3531
sakaspar <travis@travis-ci.org>
2021-01-06 18:34:28 +0100

    m

--- src/main/java/com/viraj/sample/SampleApplication.java
+++ src/main/java/com/viraj/sample/SampleApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @ContextConfiguration(locations =  {"classpath*:/spring/test-context.xml"})
 public class SampleApplication {
 
+	@GetMapping("/welcome/{name}")
+	public String wish(@PathVariable String name) {
+		return "Hi "+name+" Welcome to Doctor Service";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
+
+	
+
 }
